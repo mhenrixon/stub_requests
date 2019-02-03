0be2ff2967b7e83b13c6225c55ec06ecdb4ee43f
Mika Hel <mikael@zoolutions.se>
2019-02-03 21:18:31 +0100

    Output some helpful information

--- bin/update_docs.sh
+++ bin/update_docs.sh
@@ -12,7 +12,10 @@ command="$(echo ./!(bin|_config.yml|_index.html))"
 echo "Cleaning up current documentation: rm -rf ${command}"
 rm -rf $command
 
+echo "Copying new documentation"
 mv doc/* ./
+
+echo "Sending new documentation to github"
 git add --all
 git commit -a -m 'Update documentation'
 git push
