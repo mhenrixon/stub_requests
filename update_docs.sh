87943007e71c0679d46f9cf33944dfcd3b1fb2ae
Mika Hel <mikael@zoolutions.se>
2019-02-03 21:43:18 +0100

    Fix updating docs

---
+++ update_docs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+git checkout master
+git pull --rebase
+
+rake yard
+
+git checkout gh-pages
+
+echo "Cleaning up current documentation"
+find . ! -path '*/.git*' ! -path '*/doc*' ! -path '*/update_docs.sh*' ! -path '*/_config.yml*' ! -path '*/_index.html*' ! -path '.' | xargs rm -rf
+
+echo "Copying new documentation"
+mv doc/* ./
+
+echo "Sending new documentation to github"
+git add --all
+git commit -a -m 'Update documentation'
+git push
+
